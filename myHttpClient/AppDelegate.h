5fbea77bbc6a1678247eb4f991705c0e87d6d566
邓龙 <deng.long@myMac-mini.local>
2015-05-16 00:19:59 +0800

    Initial Commit

---
+++ myHttpClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myHttpClient
+//
+//  Created by 邓龙 on 15/5/16.
+//  Copyright (c) 2015年 邓龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myHttpClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myHttpClient
+//
+//  Created by 邓龙 on 15/5/16.
+//  Copyright (c) 2015年 邓龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
